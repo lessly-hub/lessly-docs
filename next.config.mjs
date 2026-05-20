e34372d3d8849901351a251160670739ad770bab
Artur Sabirov <artur.sabirov@apliteni.com>
2026-05-20 13:47:47 +0200

    feat(deploy): Cloudflare Workers-with-assets via @opennextjs/cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 import { createMDX } from 'fumadocs-mdx/next';
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+
+initOpenNextCloudflareForDev();
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'standalone',
 };
 
 export default withMDX(config);
